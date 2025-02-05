aad8be23c578a70538274747cbf4846a4ae381ec
Sakshi17-st <thoratsakshi2005@gmail.com>
2025-02-05 22:29:42 +0530

    Add files via upload

---
+++ Append.java
@@ -0,0 +1,8 @@
+class Append {
+    public static void main(String[] args) {
+        StringBuffer buffer = new StringBuffer("Hello");
+        buffer.append(" World");
+        System.out.println("Result: " + buffer);
+    }
+}
+
